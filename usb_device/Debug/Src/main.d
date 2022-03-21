f52ef50afef27baabe60b100c24f3c56b3aa7e5a
Nihal Sahu <nihal.sahu@uwaterloo.ca>
2022-03-21 11:53:40 -0400

    initial commit

---
+++ usb_device/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ /home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ /home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/core_cm4.h \
+ /home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/cmsis_version.h \
+ /home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/cmsis_compiler.h \
+ /home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/cmsis_gcc.h \
+ /home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/mpu_armv7.h \
+ /home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+/home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/core_cm4.h:
+/home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/cmsis_version.h:
+/home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/cmsis_compiler.h:
+/home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/cmsis_gcc.h:
+/home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Include/mpu_armv7.h:
+/home/nihal/Desktop/Projects/stm32-usb/usb_device/Inc/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
